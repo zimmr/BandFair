dde0a69989c6bd4419840470c41667faeec2833c
J. Zimmer <joaozimmr@gmail.com>
2018-12-19 16:58:53 -0200

    criadas tela de Splash e arquivo strings.xml pt-br

---
+++ app/src/main/java/zimmer/bandfair/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package zimmer.bandfair.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import zimmer.bandfair.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
